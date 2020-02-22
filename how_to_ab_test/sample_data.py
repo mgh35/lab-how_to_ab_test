f5f1d0b49888dc2906ae23133c4f659f3afdc6f8
mark <>
2020-02-22 16:04:53 -0400

    Ability to specify p and n

--- how_to_ab_test/sample_data.py
+++ how_to_ab_test/sample_data.py
@@ -1,16 +1,23 @@
 import numpy as np
 import pandas as pd
 
-def random_contingency_table_2x2(*, scale=10, is_equal_p=False):
-    p1 = np.random.uniform()
-    if is_equal_p:
-        p2 = p1
+def random_contingency_table_2x2(*, scale=10, is_equal_p=False, p=None, n=None):
+    if p is None:
+        p1 = np.random.uniform()
+        if is_equal_p:
+            p2 = p1
+        else:
+            p2 = np.random.uniform()
     else:
-        p2 = np.random.uniform()
+        p1, p2 = p
 
-    n1 = np.random.poisson(scale)
+    if n is None:
+        n1 = np.random.poisson(scale)
+        n2 = np.random.poisson(scale)
+    else:
+        n1, n2 = n
+
     s1 = np.random.binomial(n1, p1)
-    n2 = np.random.poisson(scale)
     s2 = np.random.binomial(n2, p2)
 
     return pd.DataFrame(data=[

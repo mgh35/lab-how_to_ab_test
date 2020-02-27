7606fe621480f93dcaebb95b4a9d09b27872220c
mark <>
2020-02-26 22:13:56 -0400

    Adding option to exclude tests

--- how_to_ab_test/validation.py
+++ how_to_ab_test/validation.py
@@ -7,42 +7,48 @@ from statsmodels.stats.proportion import proportions_ztest
 import how_to_ab_test.sample_data as sample_data
 
 
-def pvalues(table):
-    # _, p_fisher = scs.fisher_exact(table, alternative='two-sided')
-    p_fisher = 1.0
-    _, p_z = proportions_ztest(
-        table.Success,
-        table.Success+table.Fail,
-        alternative='two-sided',
-        prop_var=False
-    )
-    try:
-        _, p_chi2, _, _ = scs.chi2_contingency(table.values, correction=False)
-    except Exception:
-        p_chi2 = np.nan
-    try:
-        _, p_chi2_corrected, _, _ = scs.chi2_contingency(table.values, correction=True)
-    except Exception:
-        p_chi2_corrected = np.nan
+def pvalues(table, *, exclude=None):
+    if exclude is None:
+        exclude = []
+    data = []
+    cols = []
+    if not 'fisher' in exclude:
+        _, p_fisher = scs.fisher_exact(table, alternative='two-sided')
+        data.append(p_fisher)
+        cols.append('fisher')
 
-    if np.isnan(p_z):
-        p_z = 1.0
-    if np.isnan(p_chi2):
-        p_chi2 = 1.0
-    if np.isnan(p_chi2_corrected):
-        p_chi2_corrected = 1.0
+    if not 'z' in exclude:
+        _, p_z = proportions_ztest(
+            table.Success,
+            table.Success+table.Fail,
+            alternative='two-sided',
+            prop_var=False
+        )
+        if np.isnan(p_z):
+            p_z = 1.0
+        data.append(p_z)
+        cols.append('z')
+
+    if not 'chi2' in exclude:
+        try:
+            _, p_chi2, _, _ = scs.chi2_contingency(table.values, correction=True)
+        except Exception:
+            p_chi2 = np.nan
+        if np.isnan(p_chi2):
+            p_chi2 = 1.0
+        data.append(p_chi2)
+        cols.append('chi2')
 
     return pd.DataFrame(
-        data=[
-            [p_fisher, p_z, p_chi2, p_chi2_corrected],
-        ], columns=['fisher', 'z', 'chi2', 'chi2_corrected']
+        data=[data], columns=cols
     )
 
 
-def rejections(n, p1, p2, n_samples, alphas):
+def rejections(n, p1, p2, n_samples, alphas, *, exclude=None):
     samples = pd.concat([
         pvalues(
-            sample_data.random_contingency_table_2x2(n=(n, n), p=(p1, p2))
+            sample_data.random_contingency_table_2x2(n=(n, n), p=(p1, p2)),
+            exclude=exclude
         )
         for i in range(n_samples)
     ])
